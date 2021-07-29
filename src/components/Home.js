ee46d00d113d30503069a282d479490afdb560c8
JackedProgrammer <82956386+JackedProgrammer@users.noreply.github.com>
2021-07-28 22:53:31 -0400

    slight mobile fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home(){
     return (
         <main className="bg-main">
             <section className="flex justify-center main-text-display">
-                <h1 className="flex bright-accent-text text-4xl font-bold cursive leading-none justify-center lg:leading-snug home-name">Welcome, I'm Richard.</h1>
+                <h1 className="flex bright-accent-text text-6xl font-bold cursive leading-none justify-center lg:leading-snug home-name">Welcome, I'm Richard.</h1>
             </section>
         </main>
     )
