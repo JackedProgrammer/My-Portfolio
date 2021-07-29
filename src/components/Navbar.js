ce03a2301a128a5aa855802dc2c1ed278e973e5a
JackedProgrammer <82956386+JackedProgrammer@users.noreply.github.com>
2021-07-28 21:25:05 -0400

    updated navbar with color scheme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import { SocialIcon } from "react-social-icons";
+import { useState } from "react";
 
 
 export default function Navbar(){
+    const [isOpen,setIsOpen]=useState(false);
     return (
-        <nav className="bg-red-600">
+        <nav className="bg-main">
             <div className="max-w-7xl mx-auto">
                 <div className="flex justify-between">
                     <div className="flex">
@@ -14,27 +16,27 @@ export default function Navbar(){
                                 to="/" 
                                 exact 
                                 activeClassName="text-white" 
-                                className="flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                                className="flex items-center py-6 px-3 mr-4 nav-text text-4xl font-bold cursive tracking-widest">
                                 JackedProgrammer
                             </NavLink>
                         </div>
                         <div className="hidden md:flex">
                             <NavLink 
                                 to="/post"
-                                activeClassName="text-red-100 bg-red-700"
-                                className="flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                                activeClassName="nav-active"
+                                className="flex items-center py-3 px-3 my-6 rounded nav-text">
                                 Blog
                             </NavLink>
                             <NavLink
                                 to="/project"
-                                activeClassName="text-red-100 bg-red-700"
-                                className="flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                                activeClassName="nav-active"
+                                className="flex items-center py-3 px-3 my-6 rounded nav-text">
                                 Projects
                             </NavLink>
                             <NavLink 
                                 to="/about"
-                                activeClassName="text-red-100 bg-red-700"
-                                className="flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                                activeClassName="nav-active"
+                                className="flex items-center py-3 px-3 my-6 rounded nav-text">
                                 About Me
                             </NavLink>
                         </div>
@@ -45,30 +47,32 @@ export default function Navbar(){
                         <SocialIcon url="https://www.youtube.com/channel/UCbuX73IbfYsCj6EKOun1h6w" className="mr-4" target="_blank" fgColor="#fff" style={{height:35,width:35}}/>
                         <SocialIcon url="https://www.tiktok.com/@jackedprogrammer" className="mr-4" target="_blank" fgColor="#fff" style={{height:35,width:35}}/>
                     </div>
-                    <div className="flex md:hidden items-center py-3 px-3 my-6">
-                        <p>Button here</p>
+                    <div className="flex md:hidden items-center py-3 px-6 my-6">
+                        <button onClick={()=>setIsOpen(!isOpen)}>
+                            <svg class="w-6 h-6" fill="none" stroke="#86c232" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </button>
                     </div>
                 </div>
                 
             </div>
-            <div className="flex">
-                <div className="block md:hidden py-1 px-2">
+            <div>
+                <div className={`${isOpen? "block md:hidden py-1 px-2" : "hidden md:hidden py-1 px-2"}`}>
                     <NavLink 
                         to="/post"
-                        activeClassName="text-red-100 bg-red-700"
-                        className="flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        activeClassName="nav-active"
+                        className="flex items-center px-3 my-3 rounded nav-text">
                         Blog
                     </NavLink>
                     <NavLink
                         to="/project"
-                        activeClassName="text-red-100 bg-red-700"
-                        className="flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        activeClassName="nav-active"
+                        className="flex items-center px-3 my-3 rounded nav-text">
                         Projects
                     </NavLink>
                     <NavLink 
                         to="/about"
-                        activeClassName="text-red-100 bg-red-700"
-                        className="flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        activeClassName="nav-active"
+                        className="flex items-center px-3 my-3 rounded nav-text">
                         About Me
                     </NavLink>
                     <div className="flex py-3 py-3 my-6">
